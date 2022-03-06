ef8a2f166124722c741859e790fcd3ede4b27f43
bold78 <100731980+bold78@users.noreply.github.com>
2022-03-06 09:56:03 +0200

    first Commit

---
+++ src/Main_gitup.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author tades
+ *
+ */
+public class Main_gitup {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("First Commit");
+
+	}
+
+}
